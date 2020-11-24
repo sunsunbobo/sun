b4b5442bb27bc692fde182ae819a194375e73ad1
sunsunbobo <sunbo_2010@foxmail.com>
2020-11-24 14:29:58 +0800

    Add new python files for data

--- python_practice/python_data/python_yaml.py
+++ python_practice/python_data/python_yaml.py
@@ -3,3 +3,8 @@ import yaml
 print(yaml.safe_load(open("data.yaml")))
 
 print(yaml.safe_load(open("data2.yaml")))
+
+a = [[{'a': 1}, 'admin2'], 'admin3']
+
+with open("datayml.yaml", "w") as f:
+    yaml.dump(a, f)
